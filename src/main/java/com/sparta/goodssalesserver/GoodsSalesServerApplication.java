8fb679a23dca2d9c35f1b24fced81e1c9a59fe6d
hyun <kingmandoo95@gmail.com>
2024-01-10 10:00:23 +0900

    Init : application.properties 제외 나머지

---
+++ src/main/java/com/sparta/goodssalesserver/GoodsSalesServerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.goodssalesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsSalesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsSalesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/goodssalesserver/GoodsSalesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.goodssalesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsSalesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
